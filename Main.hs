5c8bd3c50706f0f3f210f85e53131111a23a73bd
aramya <22577625+thamugadi@users.noreply.github.com>
2023-07-27 12:33:03 +0100

    added convenient functions

--- Main.hs
+++ Main.hs
@@ -4,6 +4,7 @@
 
 import Data.SBV
 import Data.Word
+import Control.Monad
 
 l :: [Word8]
 l = [1..250]
@@ -13,17 +14,27 @@ f [] = []
 f [x] = [x]
 f (x:xs) = [x+5*(head xs)] ++ f xs
 
+constrIterM :: (a -> a) -> Symbolic a -> Int -> (a -> SBool) -> Symbolic ()
+constrIterM f arg n constr = arg >>= (\a -> constrIter f a n constr)
+
 constrIter :: (a -> a) -> a -> Int -> (a -> SBool) -> Symbolic ()
 constrIter f arg n constr = let iterations = take n (iterate f arg) in do
     mapM_ softConstrain $ (map constr iterations)
 
+symList :: SymVal a => [Int] -> [a] -> Symbolic [SBV a]
+symList indexes list = do
+    vars <- replicateM (length indexes) (free "SYL")
+    return $ 
+      foldl
+        (\acc (idx, val)
+          -> take idx acc ++ val : drop (idx + 1) acc) (map literal list) (zip indexes vars)
+
 search :: IO ()
 search = do
   result <- sat $ do
-    a <- free "a"
-    b <- free "b"
-    let list = [a, b] ++ (map literal (drop 2 l))
-    constrIter f list 330 (\x -> x !! 5 .== 10)
+    let list = symList [0,1] l
+    let cond = \x -> x !! 5 .== 10 .&& x !! 4 .> 1
+    constrIterM f list 300 cond 
   print result
 
 main :: IO ()
