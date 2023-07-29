b97ba2e986a3be1df734570b8b68ca43c0ed069e
aramya <22577625+thamugadi@users.noreply.github.com>
2023-07-30 00:51:11 +0100

    searching all previous states

--- Main.hs
+++ Main.hs
@@ -13,24 +13,27 @@ g (r,m) = ite (r .== 0) (1, m) $
           ite (r ./= 0) (0, m) $
           (r,m)
 
-g_inputs :: (Num a, Mergeable a, EqSymbolic a) => [a] -> Int -> (a, [a]) -> (a, [a])
-g_inputs [] 0 (c, m) = (c, m)
-g_inputs (i:is) 0 (c,m) =
-  ite (m !! 0x40 .== 0) (g_inputs (i:is) 0 (g (c, m))) $ (g_inputs (i:is) 1 (g (i, m)))
-g_inputs (i:is) 1 (c,m) =
-  ite (m !! 0x40 .== 0) (g_inputs is 2 (g (i, m))) $ (g_inputs (i:is) 1 (g (i, m)))
-g_inputs (i:is) 2 (c,m) = g_inputs (i:is) 0 (c,m)
+g_inputs :: (Num a, Mergeable a, EqSymbolic a) => [a] -> Int -> [(a, [a])] -> [(a, [a])]
+g_inputs [] 0 l = l
+g_inputs (i:is) 0 l =
+  ite (last (snd (head l)) .== 0) (g_inputs (i:is) 0 (g (i, snd (head l)) : l)) $
+  (g_inputs (i:is) 1 (g (i, snd (head l)) : l))
+g_inputs (i:is) 1 l =
+  ite (last (snd (head l)) .== 0) (g_inputs is 2 (g (i, snd (head l)) : l)) $
+  (g_inputs (i:is) 1 (g (i, snd (head l)) : l))
+g_inputs (i:is) 2 l = g_inputs (i:is) 0 l
 
-g_inputs_ :: (Num a, Mergeable a, EqSymbolic a) => [a] -> (a, [a]) -> (a, [a])
-g_inputs_ = (\x y -> g_inputs x 0 y)
+g_inputs_ :: (Num a, Mergeable a, EqSymbolic a) => [a] -> [(a, [a])] -> [(a, [a])]
+g_inputs_ l = g_inputs l 0 
 
 search_g :: IO SatResult
 search_g = do
   sat $ do
-    let list = symList [0..5] (take 5 $ repeat 0)
+    let list = symList [0..100] (take 5 $ repeat 0)
     list >>=
-      (\x -> 
-        constrain (((snd $ (g_inputs_ @(SBV Word8)) x (0, take 200 $ repeat 0)) !! 120) .== 1))
+      (\x -> do
+        let result = g_inputs_ @(SBV Word8) x [(0, take 200 $ repeat 0)]
+        constrain $ sAny (\(r, m) -> m !! 120 .== 1) result)
 
 main :: IO ()
 main = do
