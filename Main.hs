c09dc17418f2c4b9f8eec55bc795aacaab3697c7
aramya <22577625+thamugadi@users.noreply.github.com>
2023-07-30 13:59:10 +0100

    functional example

--- Main.hs
+++ Main.hs
@@ -8,7 +8,6 @@ import Data.Word
 import Control.Monad
 import Lib
 
-
 g :: (Num a, Mergeable a, EqSymbolic a) => (a, [a]) -> (a, [a])
 g (r,m) = ite (r .== 0) (1, m) $
           ite (r ./= 0) (0, m) $
@@ -17,22 +16,26 @@ g (r,m) = ite (r .== 0) (1, m) $
 g_inputs :: (Num a, Mergeable a, EqSymbolic a) => [a] -> Int -> (a, [a]) -> (a, [a])
 g_inputs [] 0 (c, m) = (c, m)
 g_inputs (i:is) 0 (c,m) =
-  ite (m !! 0x40 .== 0) (g_inputs (i:is) 0 (g (c, m))) $ (g_inputs (i:is) 1 (g (i, m)))
+  ite (m !! 0x4 .== 0) (g_inputs (i:is) 0 (g (c, m))) $ (g_inputs (i:is) 1 (g (i, m)))
 g_inputs (i:is) 1 (c,m) =
-  ite (m !! 0x40 .== 0) (g_inputs is 2 (g (i, m))) $ (g_inputs (i:is) 1 (g (i, m)))
+  ite (m !! 0x4 .== 0) (g_inputs is 2 (g (i, m))) $ (g_inputs (i:is) 1 (g (i, m)))
 g_inputs (i:is) 2 (c,m) = g_inputs (i:is) 0 (c,m)
 
 g_inputs_ :: (Num a, Mergeable a, EqSymbolic a) => [a] -> (a, [a]) -> (a, [a])
 g_inputs_ = (\x y -> g_inputs x 0 y)
 
+ar :: (Num a, Mergeable a, EqSymbolic a) => [a] -> (a, [a]) -> (a, [a])
+ar [] (c, m) = (c, c:m)
+ar (i:is) (c, m) = ar is (i, m)
+
 search_g :: IO SatResult
 search_g = do
   sat $ do
-    let list = symList [0..5] (take 5 $ repeat 0)
+    let list = symList [0..10] (take 10 $ repeat 0)
     list >>=
       (\x -> 
-        constrain (((snd $ (g_inputs_ @(SBV Word8)) x (0, take 200 $ repeat 0)) !! 120) .== 1))
+        constrain (((snd $ (ar @(SBV Word8)) x (0, take 30 $ repeat 0)) !! 0) .== 12))
 
 main :: IO ()
 main = do
-  print 1 
+  search_g >>= print 
